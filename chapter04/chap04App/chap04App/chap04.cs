894f71fdbfc31d9eac82c82fbdba0fd5c9c2a4f9
JaehyeonHeo <jhheo06@gmail.com>
2021-02-22 17:27:41 +0900

    2/22 최종 커밋

---
+++ chapter04/chap04App/chap04App/chap04.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace chap04App
+{
+    class chap04
+    {
+        static void Main(string[] args)
+        {
+            var values = 37656234;
+            if (values % 2 == 9 )
+            {
+                Console.WriteLine("짝수입니다."); 
+            }
+            else
+            {
+                Console.WriteLine("홀수입니다."); 
+            }
+
+            if (values % 7 == 0)
+            {
+                Console.WriteLine("7의 배수입니다.");
+            }
+            else
+            {
+                Console.WriteLine("7의 배수가 아닙니다.");
+            }
+
+
+        }
+    }
+}
---
+++ chapter04/chap04App/increaseTestApp/incdecTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increaseTestApp
+{
+    class incdecTest
+    {
+        static void Main(string[] args)
+        {
+            int result = 26;
+            Console.WriteLine($"현재 숫자는 {result}입니다.");
+            result += 3;
+            Console.WriteLine($"현재 숫자는 {result}입니다.");
+            result -= 10;
+            Console.WriteLine($"현재 숫자는 {result}입니다.");
+
+            Console.WriteLine($"현재 숫자는 {result++}입니다.");  // 현재라인을 실행시키고 다음줄부터 1이 증가됨 (후치)
+            Console.WriteLine($"현재 숫자는 {++result}입니다.");  // 1을 증가시키고 현재라인 실행 (전치)
+            
+        }
+    }
+}

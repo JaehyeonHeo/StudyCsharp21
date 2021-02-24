1b56357f2e51cfb60fbf8a4d40bd1bc25fb98eb9
JaehyeonHeo <jhheo06@gmail.com>
2021-02-24 17:05:14 +0900

    2/24 7장 커밋

---
+++ chapter07/Chap07App/AccessModifierTestApp/MainApp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierTestApp
+{
+    class Boiler
+    {
+        public int temp ;   //물온도     
+        /* internal(빈도 낮음)  
+         * public, protected, private (빈도 높음)*/
+
+        public void SetTemp(int temp)
+        {
+            if (temp < 30 || temp > 60) 
+            {
+                Console.WriteLine("물온도 이상, 다시 셋팅바립니다. 59도로 맞춥니다");
+                this.temp = 59; 
+                return; 
+            }
+            else
+            {
+                this.temp = 59; 
+            }
+            this.temp = temp; 
+        }
+        public int GetTemp()
+        {
+            return this.temp; 
+        }
+        public void TurnOnBoiler()
+        {
+            Console.WriteLine("보일러를 켭니다"); 
+        }
+        public void TurnOffBoiler()
+        {
+            Console.WriteLine("보일러를 끕니다"); 
+        }
+    }
+
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Boiler kitturami = new Boiler();
+            var currTemp = kitturami.GetTemp(); 
+                Console.WriteLine($"현재 온도는 {currTemp}℃ 입니다.");
+            kitturami.SetTemp(40); 
+            kitturami.TurnOnBoiler();
+            kitturami.SetTemp(59);
+            
+
+            if (kitturami.GetTemp() >= 59)
+            {
+                kitturami.TurnOffBoiler(); 
+            }
+        }
+    }
+}
--- chapter07/Chap07App/Chap07App/objectTest.cs
+++ chapter07/Chap07App/Chap07App/objectTest.cs
@@ -6,11 +6,11 @@ namespace Chap07App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("객체 사용");
+            /*Console.WriteLine("객체 사용");
             //고양이 객체의 실체(instance) 생성
-            /* Cat cat1 = new Cat();      
+            *//* Cat cat1 = new Cat();      
              cat1.Name = "야옹이";
-             cat1.Color = "흰색";*/
+             cat1.Color = "흰색";*//*
             Cat cat1 = new Cat("야옹이", "흰색"); 
             cat1.Meow();
 
@@ -22,29 +22,39 @@ namespace Chap07App
             Cat nero = new Cat();
             nero.Name = "검고 네로";
             nero.Color = "검은색";
-            nero.Meow(); 
+            nero.Meow(); */
+
+            Cat yomi = new Cat();
+            yomi.Name = "YOMI";
+            yomi.Color = "BLACK"; 
+            yomi.Gender = "MALE"; 
+            yomi.Meow();
         }
     }
 
     class Cat
     {
         //개체속성
-        public Cat()   //사용자 정의 생성자
+        public Cat(string Name)     //사용자 정의 생성자 --> 기본생성자 만들어줘야한다 
         {
-
+            this.Name = Name;    
         }
         
-        public Cat(string _name, string _color)
+        public Cat(string Name, string Color) 
         {
-            Name = _name;
-            Color = _color;
+            this.Name = Name;        // this 사용 ==> 중복된 생성자 이름을 지칭하기 위해서 사용 
+            this.Color = Color;
         }
 
-        public Cat(string _name, string _color, string _gender)
+        public Cat(string Name, string Color, string Gender)
         {
-            Name = _name;
-            Color = _color;
-            Gender = _gender;
+            this.Name = Name;
+            this.Color = Color;
+            this.Gender = Gender;
+        }
+
+        public Cat()
+        {
         }
 
         public string Name;
@@ -54,7 +64,7 @@ namespace Chap07App
         //행위속성 
         public void Meow()
         {
-            Console.WriteLine($"{Name} : 야옹"); 
+            Console.WriteLine($"{this.Name} (색상 {this.Color}) / (성별 : {this.Gender}) : 야옹 !"); 
         }
     }
 }

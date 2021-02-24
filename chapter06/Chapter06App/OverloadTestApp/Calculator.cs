7be07602820157b2f1328d26b231311648213fb7
JaehyeonHeo <jhheo06@gmail.com>
2021-02-24 10:41:16 +0900

    6장 커밋

---
+++ chapter06/Chapter06App/OverloadTestApp/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OverloadTestApp
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("계산기 !");
+
+            int x = Calculator.plus(3, 4);
+            Console.WriteLine($"3 + 4 = {x}");
+
+            float y = Calculator.plus(3.14f, 5.6f);
+            Console.WriteLine($"3.14 + 5.6 = {y}");
+
+            Console.WriteLine($"3.14 + 5.6 = {Calculator.plus(3.14, 5.6)}");
+
+            Console.WriteLine($"3.14 + 5 = {Calculator.plus(3.14, "5")}");
+
+            int z = Calculator.sum(3, 4, 5);
+            Console.WriteLine($"{z}");
+
+            int total = 0;
+            total = sum(1, 2, 4, 5);
+            Console.WriteLine($"합계는 {total}");
+
+            Console.WriteLine($"10까지의 합은 : {sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}"); 
+
+        }
+
+        
+        private static int sum(params int[] args)
+        {
+            int result = 0; 
+
+            foreach (var arg in args)
+            {
+                result += arg; 
+            }
+            return result; 
+        }
+
+        
+
+        private static int plus(int v1, int v2)
+        {
+            int result = v1 + v2;
+            return result; 
+        }
+    }
+}
--- chapter06/Chapter06App/ProfileTestApp/profileTest.cs
+++ chapter06/Chapter06App/ProfileTestApp/profileTest.cs
@@ -12,11 +12,14 @@ namespace ProfileTestApp
         {
             printProfile("성명건", "010-6683-7732");
 
-            printProfile(null, "010-1111-2222"); 
+            printProfile(null, "010-1111-2222");
 
+            // printProfile(phone: "010-9999-9999", name: "홍길동");
+            // printProfile(name : "홍길순", phone : "010-8888-8888");
+            printProfile("최백호"); 
         }
 
-        private static void printProfile(string name, string phone)
+        private static void printProfile(string name, string phone = "010-7979-7979")
         {
             // 프로필 출력
             if (string.IsNullOrEmpty(name))
@@ -25,6 +28,7 @@ namespace ProfileTestApp
                 return;   // 호출한 곳으로 돌아간다
             }
             Console.WriteLine($"이름 : {name}, 폰번호 : {phone}");
+            return; 
 
         }
     }

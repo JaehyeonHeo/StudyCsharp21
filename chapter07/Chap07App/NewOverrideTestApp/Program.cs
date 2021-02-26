bcb84ec86e4791d9255057605ee2576b97d9dc65
JaehyeonHeo <jhheo06@gmail.com>
2021-02-26 13:36:53 +0900

    2/26 오전 커밋

---
+++ chapter07/Chap07App/NewOverrideTestApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewOverrideTestApp
+{
+    class Mammal
+    {
+        public string Name { get; set; }
+
+        public void Breathe()
+        {
+            Console.WriteLine($"{this.Name}이(가) 숨을 쉰다."); 
+        }
+
+        public virtual void Move()   // virtual 사용
+        {
+            Console.WriteLine($"(부모작업) {this.Name}이(가) 이동한다."); 
+        }
+    }
+    class Dog : Mammal
+    {
+       
+       /* public void Move()
+        {
+            Console.WriteLine($"{this.Name}이(가) 네발로 달린다."); 
+        }*/
+        public override void Move()
+        {
+            base.Move();
+            Console.WriteLine($"{this.Name}이(가) 헤엄쳐서 움직인다.");
+        }
+    }
+
+    class Human : Mammal
+    {
+        public new void Move() // new를 사용하면 부모의 메서드 숨긴다.(자식의 메서드만 사용) 
+        {
+            Console.WriteLine($"{this.Name}이(가) 두발로 이동한다."); 
+        }
+    }
+
+    class Whale : Mammal
+    {
+        public override void Move()  // override를 사용하면 부모의 메서드를 재정의 한다. 
+        {
+            Console.WriteLine($"{this.Name}이(가) 헤엄쳐서 움직인다.");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog ppoppi = new Dog();
+            ppoppi.Name = "뽀삐"; 
+            ppoppi.Move();
+
+            Human mansigi = new Human();
+            mansigi.Name = "만식이"; 
+            mansigi.Move();
+
+            Whale whale = new Whale();
+            whale.Name = "고래"; 
+            whale.Move(); 
+
+        }
+    }
+}
---
+++ chapter07/Chap07App/NewTypeCastApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTypeCastApp
+{
+    class Mammal //포유류
+    {
+        public string Name { get; set; }    //Property(프로퍼티) 사용
+
+        public void Nurse()
+        {
+            Console.WriteLine($"{this.Name}을(를) 키우다"); 
+        }
+    }
+
+    class Dog : Mammal  //강아지
+    {
+        public void Bark()
+        {
+            Console.WriteLine($"{this.Name} : 멍멍!!"); 
+        }
+    }
+
+    class Cat : Mammal  //고양이
+    {
+        public void Meow()
+        {
+            Console.WriteLine($"{this.Name} : 야옹!!"); 
+        }
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Mammal mammal = new Mammal();
+            mammal.Name = "포유류";
+            mammal.Nurse();
+
+            Dog ppoppi = new Dog();
+            ppoppi.Name = "강아지";
+            ppoppi.Nurse();
+            ppoppi.Bark();
+
+            Cat chichi = new Cat();
+            chichi.Name = "고양이";
+            chichi.Nurse();
+            chichi.Meow();
+
+
+            if (ppoppi is Mammal)   // ppoppi는 Mammal 인가?
+            {
+                Mammal mammal1 = ppoppi ;  // 참조 형식은 as를 써서 형변환해야한다
+                mammal1.Nurse(); 
+               // mammal1.Bark();   --> 에러; 안된다 (부모형식으로 형변환했기 때문에 이제 자식클래스에 있는 메소드 사용불가)
+            }
+
+            
+        }
+    }
+}

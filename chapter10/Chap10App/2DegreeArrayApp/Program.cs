5d8f3dfd732f251dbeed80747521add13f1d2b4f
JaehyeonHeo <jhheo06@gmail.com>
2021-02-26 17:20:45 +0900

    2/26 최종 커밋

---
+++ chapter10/Chap10App/2DegreeArrayApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DegreeArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2차원 배열
+
+            int[,] arr = new int[2, 3];
+            arr[0, 0] = 1;
+            arr[0, 1] = 2;
+            arr[0, 2] = 3;
+            arr[1, 0] = 4;
+            arr[1, 1] = 5;
+            arr[1, 2] = 6;
+            // 위와 동일 (간결하게 쓰는 방법)  ==>   int[,] arr = { {1,2,3},  {4,5,6} };
+            
+
+            // 출력
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"[{i} ,{j}] : {arr[i, j]}"); 
+                }
+                Console.WriteLine(""); 
+            }
+        }
+    }
+}
---
+++ chapter10/Chap10App/Chap10App/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap10App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            array[0] = 80;
+            array[1] = 74;
+            array[2] = 81;
+            array[3] = 90;
+            array[4] = 34;
+
+            // for문 사용해서 출력 !!
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}"); 
+            }
+
+            // foreach문 사용해서 출력 !!
+            /*var idx = 0;
+            foreach (var item in array)
+            {
+                Console.WriteLine($"{idx++}번째 값 : {item}");
+            }*/
+
+            Console.WriteLine("오름차순 후"); 
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            Console.WriteLine("내림차순 후");
+            Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            int idx = Array.IndexOf(array, 34);
+            Console.WriteLine($"34의 인덱스 : {idx}");
+
+            Array.Clear(array, 3, 2);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+
+
+            Console.WriteLine("다른배열"); 
+            // 좀 더 간결한 배열 
+            int[] array2 = { 80, 74, 81, 90, 34 };
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array2[i]}");
+            }
+            
+        }
+    }
+}

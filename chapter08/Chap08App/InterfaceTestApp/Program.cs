0a5680a297a41aab4f93fdc8cd5d34883c5fe68e
JaehyeonHeo <jhheo06@gmail.com>
2021-02-26 15:53:20 +0900

    8장 인터페이스

---
+++ chapter08/Chap08App/InterfaceTestApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTestApp
+{
+   /* class Ridable
+    {
+        public virtual void Ride()  // 부모:virtual, 자식:override
+        {
+            Console.WriteLine("탈것!"); 
+        }
+    } */
+
+    interface ICar 
+    {
+        void Run();
+
+        void Ride(); 
+        
+    }
+
+    interface IPlane 
+    {
+        void Fly();
+
+        void Ride(); 
+        
+    }
+
+    class DroneCar : IPlane, ICar     // 다중상속의 문제점 해결 !!
+    {
+        public void Fly()
+        {
+            Console.WriteLine("드론카 날다!"); 
+        }
+
+        public void Ride()                             // Ride가 Iplane, Icar 두 곳에 다 있어도 하나만 구현 ! 
+        {                                              //(인터페이스에는 인스턴스가 없기 때문에 상속받은 클래스에서 구현
+            Run();
+            Fly(); 
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("드론카 달리다!"); 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DroneCar dreamCar = new DroneCar();
+            dreamCar.Ride();
+
+        }
+    }
+}
---
+++ chapter08/Chap08App/LoggerTestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerTestApp
+{
+    interface ILogger
+    {
+        void Writelog(string message); 
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        
+        public void Writelog(string message)
+        {
+            Console.WriteLine($"{DateTime.Now} : {message}"); 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("consoleLogger 테스트");
+            ConsoleLogger logger = new ConsoleLogger();
+            logger.Writelog("로그 메시지"); 
+
+        }
+    }
+}

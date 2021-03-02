d9d80cd56343d79ea27b706f336ed54b97bf7ac1
JaehyeonHeo <jhheo06@gmail.com>
2021-03-02 12:24:23 +0900

    11장 커밋

---
+++ chapter11/Chap11App/Chap11App/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap11App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] source = { 11, 21, 33, 45, 56 };  // 5개 int 배열
+            int[] target = new int[source.Length];  // 5개 int 배열 초기화
+
+            CoppyArray(source, target);     // int 배열 복사 
+            Console.WriteLine("배열복사");
+            foreach (var item in target)
+            {
+                Console.WriteLine(item); 
+            }
+
+            string[] source2 = { "하나", "둘", "셋", "넷", "다섯", "여섯" };
+            string[] target2 = new string[source2.Length];
+
+            CoppyArray(source2, target2);
+            Console.WriteLine("string 복사");
+            foreach (string item in source2)
+            {
+                Console.WriteLine(item); 
+            }
+
+            float[] source3 = { 1.1f, 2.2f, 3.3f, 4.5f, 6.4f };
+            float[] target3 = new float[source3.Length];
+
+            CoppyArray(source3, target3);
+            Console.WriteLine("float 복사");
+            foreach (float item in source3)
+            {
+                Console.WriteLine(item); 
+            }
+
+
+        }
+
+        private static void CoppyArray(float[] source3, float[] target3)
+        {
+            for (int i = 0; i < source3.Length; i++)
+            {
+                target3[i] = source3[i]; 
+            }
+        }
+
+        private static void CoppyArray(string[] source2, string[] target2)
+        {
+            for (int i = 0; i < source2.Length; i++)
+            {
+                target2[i] = source2[i]; 
+            }
+        }
+
+        private static void CoppyArray(int[] source, int[] target)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                target[i] = source[i]; 
+            }
+        }
+    }
+}
---
+++ chapter11/Chap11App/GenericCopyArrayApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCopyArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] source = { 11, 21, 33, 45, 56 };  // 5개 int 배열
+            int[] target = new int[source.Length];  // 5개 int 배열 초기화
+
+            CoppyArray(source, target);     // int 배열 복사 
+            Console.WriteLine("배열복사");
+            foreach (var item in target)
+            {
+                Console.WriteLine(item);
+            }
+
+            string[] source2 = { "하나", "둘", "셋", "넷", "다섯", "여섯" };
+            string[] target2 = new string[source2.Length];
+
+            CoppyArray(source2, target2);
+            Console.WriteLine("string 복사");
+            foreach (string item in source2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+        // 일반화 클래스 : <T> 또는 <P> 사용하면 메서드 한개로 모든 타입 반환가능 
+        private static void CoppyArray<T>(T[] source, T[] target) 
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                target[i] = source[i];
+            }
+        }
+    }
+}

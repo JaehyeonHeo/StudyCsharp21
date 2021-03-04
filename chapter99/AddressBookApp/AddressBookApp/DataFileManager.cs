cd5213869e1d8c22c6a19ec98f6f6a57553bf384
JaehyeonHeo <jhheo06@gmail.com>
2021-03-04 16:13:43 +0900

    3/4 주소록프로그램 커밋

---
+++ chapter99/AddressBookApp/AddressBookApp/DataFileManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace AddressBookApp
+{
+    class DataFileManger
+    {
+        // 텍스트 파일에서 저장된 주소록을 불러온다(로드)
+        //D:\DEV\GitRepository\StudyCsharp21\chapter99\AddressBookApp\AddressBookApp\bin\Debug\netcoreapp3.1  ==> 현재 실행파일 주소
+        const string dataFileName = "address.dat";
+
+        public void ReadData(List<AddressInfo> param)
+        {
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName; // 데이터파일 생성
+            StreamReader sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+            while (sr.EndOfStream == false)
+            {
+                var temp = sr.ReadLine();
+                // temp잘라서 manager.listAddress 할당 
+                string[] splits = temp.Split("|");
+                param.Add(new AddressInfo() { Name = splits[0], Phone = splits[1], Address = splits[2] });
+            }
+            sr.Close();
+        }
+        public void WriteData(List<AddressInfo> param)
+        {
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName; // 데이터파일 생성
+            /* 담겨있는 주소록 데이터를 파일에 저장한다. ==> 실행파일과 같은 폴더에 있어야함 */
+            StreamWriter sw = new StreamWriter(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write));
+            if (param.Count > 0)
+            {
+                foreach (var item in param)
+                {
+                    sw.WriteLine($"{item.Name}|{item.Phone}|{item.Address}");
+                }
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ chapter99/AddressBookApp/AddressBookApp/MainApp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AddressBookApp
+{
+    // 매인 
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            try // try-catch로 예외처리 
+            {
+                AddressManager manager = new AddressManager();
+                manager.listAddress = new List<AddressInfo>();
+
+                DataFileManger fileManger = new DataFileManger();
+                fileManger.ReadData(manager.listAddress);
+
+
+                while (true)
+                {
+                    Console.Clear();
+
+                    manager.PrintMenu();
+                    int menuNum = manager.SelectMenu();
+
+                    switch (menuNum)
+                    {
+                        case 1:  // 주소입력
+                            Console.Clear();
+                            manager.InputAddress();
+                            break;
+
+                        case 2:  // 주소검색
+                            Console.Clear();
+                            manager.SearchAddress();
+                            break;
+
+                        case 3:  //주소수정
+                            Console.Clear();
+                            manager.UpdateAddress();
+                            break;
+
+                        case 4:  //주소삭제
+                            Console.Clear();
+                            manager.DeleteAddress();
+                            break;
+
+                        case 5:  //주소 전체 출력
+                            Console.Clear();
+                            manager.PrintAlladdress();
+                            break;
+                        case 6:  // 프로그램 종료
+
+                            fileManger.WriteData(manager.listAddress);
+
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}"); 
+                throw;
+            }
+            
+        }
+
+
+
+
+    }
+}

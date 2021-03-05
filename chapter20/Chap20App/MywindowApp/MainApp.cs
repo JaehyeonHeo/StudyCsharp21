6a68a8a995f97e9101ad4c9781f8b2b45c6e5c6d
JaehyeonHeo <jhheo06@gmail.com>
2021-03-05 16:54:32 +0900

    3/5 윈폼 시작

---
+++ chapter20/Chap20App/MywindowApp/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MywindowApp
+{
+    class MainApp : Form
+    {
+        public MainApp(string title)
+        {
+            this.Text = title; 
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Application.Run(new MainApp("Mouse Event App")); 
+        }
+    }
+
+    
+}
---
+++ chapter20/Chap20App/UsingControlsApp/FrmMain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace UsingControlsApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            var FontsList = FontFamily.Families;
+            foreach (var font in FontsList)
+            {
+                CboFont.Items.Add(font.Name); 
+            }
+        }
+
+        private void ChangeFont() // ==> 콤보박스, 체크박스 값변경으로 글자체 변경하는 메서드 
+        {
+            if (CboFont.SelectedIndex < 0) return;    // 콤보선택값의 인덱스가 없으면 메서드 탈출 
+
+            FontStyle style = FontStyle.Regular;
+            if (ChkBold.Checked) style |= FontStyle.Bold;
+            if (ChkItalic.Checked) style |= FontStyle.Italic;
+
+            TxtResult.Font = new Font((string)CboFont.SelectedItem, 14, style); 
+        }
+
+        private void CboFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFont(); 
+        }
+
+        private void ChkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void ChkItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void TrbHandle_Scroll(object sender, EventArgs e)
+        {
+            PrbDisplay.Value = TrbHandle.Value; 
+        }
+    }
+}
